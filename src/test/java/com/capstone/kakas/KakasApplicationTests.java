43d1edca4254c2b8c17c54eeccf353d90fd430c9
임동재 <djlim1998@gmail.com>
2025-05-07 23:46:00 +0900

    Initialize Spring Boot project

---
+++ src/test/java/com/capstone/kakas/KakasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.kakas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
