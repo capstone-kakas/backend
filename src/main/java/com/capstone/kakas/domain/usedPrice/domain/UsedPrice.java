7adcd86ae078bd481c8d62901f54d9c05940f17c
임동재 <djlim1998@gmail.com>
2025-05-16 20:31:15 +0900

    feat: crawling 도메인파일 추가
    - application.yml

---
+++ src/main/java/com/capstone/kakas/domain/usedPrice/domain/UsedPrice.java
@@ -0,0 +1,11 @@
+package com.capstone.kakas.domain.usedPrice.domain;
+
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UsedPrice {
+}
