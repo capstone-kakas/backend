1aa1b61d90eb14cfac0fff465c9086b63b10bc90
임동재 <djlim1998@gmail.com>
2025-05-27 01:15:12 +0900

    #7 feat: Application EnableJpaAuditing 추가

--- src/main/java/com/capstone/kakas/KakasApplication.java
+++ src/main/java/com/capstone/kakas/KakasApplication.java
@@ -2,8 +2,10 @@ package com.capstone.kakas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KakasApplication {
 
 	public static void main(String[] args) {
