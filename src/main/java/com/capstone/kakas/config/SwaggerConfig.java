1b84299b68ac6ab6b0234fb2fd3c7e5fe68ad6b5
임동재 <djlim1998@gmail.com>
2025-05-27 13:41:27 +0900

    #10 feat: SwaggerConfig 추가

---
+++ src/main/java/com/capstone/kakas/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.capstone.kakas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI umcOpenAPI() {
+        final String JWT_SCHEME_NAME = "JWT TOKEN";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UMC Server WorkBook API")
+                        .description("UMC Server WorkBook API 명세서")
+                        .version("1.0.0")
+                )
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(new SecurityRequirement().addList(JWT_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(JWT_SCHEME_NAME, new SecurityScheme()
+                                .name(JWT_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
