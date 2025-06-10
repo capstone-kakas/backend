df5dd05df459dbbda159830b6173d313603be879
임동재 <djlim1998@gmail.com>
2025-06-10 21:22:15 +0900

    #34 fix: WebClientConfig 수정

--- src/main/java/com/capstone/kakas/global/config/WebClientConfig.java
+++ src/main/java/com/capstone/kakas/global/config/WebClientConfig.java
@@ -1,15 +1,30 @@
 package com.capstone.kakas.global.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 180000) // 60초 연결 타임아웃
+                .responseTimeout(Duration.ofMinutes(5)) // 5분 응답 타임아웃
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(300)) // 5분 읽기 타임아웃
+                                .addHandlerLast(new WriteTimeoutHandler(300))); // 5분 쓰기 타임아웃
+
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024))
                 .build();
     }
